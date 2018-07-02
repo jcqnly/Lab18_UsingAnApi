8e5ad7a5d424ff1b98d1fc0592658ce488800e82
jcqnly <jcqnly@gmail.com>
2018-07-01 18:31:35 -0700

    basic MVC scaffolding complete

---
+++ UseTodoApi/UseTodoApi/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UseTodoApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Directs the user to the index page where the user can
+        /// view the Todo lists and items for each list
+        /// </summary>
+        /// <returns></returns>
+        //[HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
